3cfcb9c0f77260fb604f4f950be610392c45dbb4
yoshuro <yoshuro@hotmail.com>
2018-06-14 13:11:30 +0200

    Finish initial cli part

--- cli.ts
+++ cli.ts
@@ -1,10 +1,19 @@
 import * as minimist from 'minimist'
+import * as isUrl from 'is-url'
 
-// import { Config } from './config'
-// import config from './config.json'
+import main from './'
+
+const config = require('./config.json')
 
 function printHelp() {
-  console.log('help')
+  console.log('Lane --help')
+  console.log('Usage:')
+  console.log('$ lane https://google.com -k search -c 5m')
+  console.log('')
+  console.log('Flags:')
+  console.log('-k, --keywords -> Comma separated list of keywords')
+  console.log('-c --continuous -> Interval between calls (can be entered as a number in ms or like `1h`, `5m`)')
+  console.log('Without -c flag it will check the site only once')
 }
 
 function printVersion() {
@@ -19,13 +28,53 @@ function run() {
 
   if (argv.version || argv.v) {
     printVersion()
-    return
+    process.exit()
   }
 
   if (!cmd || cmd.toLowerCase() === 'help') {
     printHelp()
-    return
+    process.exit()
   }
+
+  // Check if any of the required parameters is missing
+  // Required: url, keywords
+  // We already know url exists, so only keywords are checked
+
+  const keywords: string = argv.k || argv.keywords
+
+  if (!keywords) {
+    console.error('The argument `keywords` (-k, --keywords) is required')
+    process.exit(1)
+  }
+
+  // Check if cmd is a valid url
+  if (!isUrl(cmd)) {
+    console.error('The first argument must be a valid url')
+    process.exit(1)
+  }
+
+  // Convert keywords to an array and remove whitespace
+  const splittedKeywords = keywords
+    .split(',')
+    .map(keyword => keyword.trim())
+
+  // Update config with flags
+  const updatedConfig = Object.assign({}, config, {
+    uri: cmd,
+    keywords: splittedKeywords,
+  })
+
+  updatedConfig.notify = argv.n || argv.notify || config.notify
+
+  const continuous = argv.c || argv.continuous || config.continuous
+
+  if (continuous) {
+    updatedConfig.continuous = true
+    updatedConfig.runEvery = continuous
+  }
+
+  // Everything seems ok, pass to main function
+  main(updatedConfig)
 }
 
 export default run
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { Config } from './config'
 
 function main(args: Config) {
-
+  console.log('Hello from main')
+  console.log('Config:', args)
 }
 
 export default main
