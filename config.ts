406319bf44f690e5a3a186ecebdf4039fd10b439
yoshuro <yoshuro@hotmail.com>
2018-06-14 12:28:43 +0200

    Mark optional types in config

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export interface Config {
   uri: string,
   keywords: Array<string>,
-  continuous: boolean,
-  runEvery: number | string,
-  notify: boolean,
+  continuous?: boolean,
+  runEvery?: number | string,
+  notify?: boolean,
 }
